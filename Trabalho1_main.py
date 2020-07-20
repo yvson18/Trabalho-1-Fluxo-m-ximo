97e5f54b2be4060192475913fc16980d66680dd2
Miguel <miguelrodrigues@eng.ci.ufpb.br>
2020-07-20 18:13:56 -0300

    Update Trabalho1_main.py

--- Trabalho1_main.py
+++ Trabalho1_main.py
@@ -4,20 +4,20 @@ from ortools.linear_solver import pywraplp
 
 def Trabalho1_main():
 
-    solver = pywraplp.Solver('Trabalho1_main', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+    solver = pywraplp.Solver('Trabalho1_main', pywraplp.Solver.GLOP_LINEAR_PROGRAMMING) #Definindo o solver como variável
 
-    _N_M_S_T_, _i_j_c_m_ = processamento_dos_dados('problema.txt')
+    _N_M_S_T_, _i_j_c_m_ = processamento_dos_dados('problema.txt') #Lendo o arquivo desejado (.txt)
     
-    i_do_arco_m = [origem[0] for origem in _i_j_c_m_]
-    j_do_arco_m = [fim[1] for fim in _i_j_c_m_]
-    c_m_do_arco = [fluxos[2] for fluxos in _i_j_c_m_]
+    i_do_arco_m = [origem[0] for origem in _i_j_c_m_] #Array i
+    j_do_arco_m = [fim[1] for fim in _i_j_c_m_] #Array j
+    c_m_do_arco = [fluxos[2] for fluxos in _i_j_c_m_] #Array custo
 
 
     numero_de_nos = _N_M_S_T_[0]
     numero_de_arcos = _N_M_S_T_[1]
 
-    varSolver = []
-    restricoes = []
+    varSolver = [] #Array dos arcos como variáveis
+    restricoes = [] #Array das constraints
 
     
     #Criando as variáveis de cada arco para o solver
@@ -28,7 +28,8 @@ def Trabalho1_main():
 
     #Criação do arco que liga o nó escoadouro até o nó de entrada
     varSolver.append(solver.NumVar(0, solver.infinity(), 'X'))
-
+    
+    #Definindo as restrições
     cont = 2
     cont_aux = 0
     while(cont <= numero_de_nos):
@@ -53,12 +54,13 @@ def Trabalho1_main():
             restricoes.append(constraint)
             del constraint   
 
+    #Definindo a função objetivo
     objective = solver.Objective()
-    objective.SetCoefficient(varSolver[len(varSolver)-1], -1)
-    objective.SetMinimization()
+    objective.SetCoefficient(varSolver[len(varSolver)-1], -1) #Definindo Z = -X
+    objective.SetMinimization() #Definindo min Z
 
-    solver.Solve()
-    opt_solution = varSolver[len(varSolver)-1].solution_value()
+    solver.Solve() #Acionando o solver
+    opt_solution = varSolver[len(varSolver)-1].solution_value() #Solução ótima
     print('Número de arcos =', solver.NumVariables())
     print('Número de restrições =', solver.NumConstraints())
     print('Solução (Arcos enumerados):')
